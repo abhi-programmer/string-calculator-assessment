24ae6e0b6e227f8dd701644ea03b237f0057f184
amanchoudhary16 <aman.choudhary@softedge.in>
2026-02-02 22:27:46 +0530

    6 ➜ new line changes

--- lib/string_calculator.dart
+++ lib/string_calculator.dart
@@ -2,7 +2,18 @@ class StringCalculator {
   int add(String numbers) {
     if (numbers.isEmpty) return 0;
 
-    final parts = numbers.split(RegExp(',|\n'));
+    String numbersPart = numbers;
+    String delimiterPattern = ',|\n';
+
+    if (numbers.startsWith('//')) {
+      final lines = numbers.split('\n');
+      final delimiter = lines.first.substring(2);
+
+      delimiterPattern = RegExp.escape(delimiter);
+      numbersPart = lines.sublist(1).join('\n');
+    }
+
+    final parts = numbersPart.split(RegExp(delimiterPattern));
 
     int sum = 0;
     for (final p in parts) {
--- test/string_calculator_test.dart
+++ test/string_calculator_test.dart
@@ -26,4 +26,9 @@ void main() {
     final calc = StringCalculator();
     expect(calc.add('1\n2,3'), 6);
   });
+
+  test('supports custom delimiter', () {
+    final calc = StringCalculator();
+    expect(calc.add('//;\n1;2'), 3);
+  });
 }
