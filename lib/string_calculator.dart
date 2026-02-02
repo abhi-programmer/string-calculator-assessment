eff781a58e78cea7a9c9b5b230bf956d5a2e1a24
amanchoudhary16 <aman.choudhary@softedge.in>
2026-02-02 22:24:54 +0530

    5 ➜ Custom delimiter

--- lib/string_calculator.dart
+++ lib/string_calculator.dart
@@ -2,7 +2,7 @@ class StringCalculator {
   int add(String numbers) {
     if (numbers.isEmpty) return 0;
 
-    final parts = numbers.split(',');
+    final parts = numbers.split(RegExp(',|\n'));
 
     int sum = 0;
     for (final p in parts) {
--- test/string_calculator_test.dart
+++ test/string_calculator_test.dart
@@ -21,4 +21,9 @@ void main() {
     final calc = StringCalculator();
     expect(calc.add('1,2,3,4'), 10);
   });
+
+  test('numbers separated by new line and comma', () {
+    final calc = StringCalculator();
+    expect(calc.add('1\n2,3'), 6);
+  });
 }
